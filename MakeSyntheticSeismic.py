1e6b4b23af364c9ff1d5167c2af40b55c831b556
greveley <66331857+Greveley@users.noreply.github.com>
2021-03-02 13:33:43 +0000

    Allowing for variable image size

--- MakeSyntheticSeismic.py
+++ MakeSyntheticSeismic.py
@@ -4,7 +4,7 @@ from pylops.utils.seismicevents import makeaxis, linear2d
 from pylops.utils.wavelets import ricker
 import random
 
-def MakeSeismic(samples):
+def MakeSeismic(samples,img_size=128,freq_low=5,freq_high=30,num_events=6):
 
     """Simple generation of noisy synthetic linear seismic events. 
         Input:
@@ -20,8 +20,8 @@ def MakeSeismic(samples):
     noisy_signal = []
 
     # Parameters for the seismic canvas
-    par = {'ox':0, 'dx':12.5, 'nx':128, # offsets
-       'ot':0, 'dt':0.004, 'nt':128, # time
+    par = {'ox':0, 'dx':12.5, 'nx':img_size, # offsets
+       'ot':0, 'dt':0.004, 'nt':img_size, # time
        'f0': random.randint(5,30), 'nfmax': 50}
     
     # Make canvas
@@ -31,20 +31,33 @@ def MakeSeismic(samples):
 
     # Parameters for events
     v = 1500
-    ang_range = 70
+    ang_range = 50
     amp_range = 2
     i = 0
-    while i <= samples: 
-        # Time of events
-        t0 = [random.uniform(t.min(),t.max())*0.8, random.uniform(t.min(),t.max())*0.8, random.uniform(t.min(),t.max())*0.8]
-        # Angle of events
-        theta = [random.uniform(-ang_range,ang_range),random.uniform(-ang_range,ang_range),random.uniform(-ang_range,ang_range)]
-        # Ampltidues of events
-        amp = [random.uniform(-amp_range,amp_range), random.uniform(-amp_range,amp_range), random.uniform(-amp_range,amp_range)]
+    amp_lim = 0.5
+    while i < samples: 
+        iEv = 0
+        t0 = []
+        theta = []
+        amp = []
+        while iEv <= num_events:
+            # Time of events
+            t0.append(random.uniform(t.min(),t.max())*0.7) 
+            # Angle of events
+            theta.append(random.uniform(-ang_range,ang_range))
+            # Amplitude of events
+            amp.append(random.uniform(-amp_range,amp_range))
+            # clipping events to be above -0.2 and 0.2 
+            if amp[iEv]<0:
+                amp[iEv] = np.min([-amp_lim,amp[iEv]])
+            else: 
+                amp[iEv] = np.max([amp_lim,amp[iEv]])
+            iEv+=1
+        
         # Making events
         mlin, mlinwav = linear2d(x, t, v, t0,theta, amp, wav)
         # Creating noise
-        n = np.random.normal(loc=0,scale=1.0,size=mlinwav.shape)*random.uniform(-0.7,0.7)
+        n = np.random.normal(loc=0,scale=1.0,size=mlinwav.shape)*random.uniform(-0.5,0.5)
         s = mlinwav
         # Adding noise
         ns = s+n
@@ -54,23 +67,27 @@ def MakeSeismic(samples):
         noisy_signal.append(ns)
         i +=1
 
-    return np.array(clean_signal), np.array(noise), np.array(noisy_signal)
+
+
+    return  np.array(clean_signal).reshape(samples,img_size,img_size,1),np.array(noise).reshape(samples,img_size,img_size,1),np.array(noisy_signal).reshape(samples,img_size,img_size,1)
 
 def PlotSeis(data, num=0, save=False):
+    # print (np.data[0])
+    size = np.array(data[0]).shape[1]
 
         # Parameters for the seismic canvas
-    par = {'ox':0, 'dx':12.5, 'nx':128, # offsets
-       'ot':0, 'dt':0.004, 'nt':128, # time
+    par = {'ox':0, 'dx':12.5, 'nx':size, # offsets
+       'ot':0, 'dt':0.004, 'nt':size, # time
        'f0': random.randint(5,30), 'nfmax': 50}
     
     # Make canvas
     t, t2, x, y = makeaxis(par)
 
-    fig, axs = plt.subplots(1, len(data), figsize=(12, 5))
+    fig, axs = plt.subplots(1, len(data), figsize=(len(data*4), 7))
     # Looping over datasets to compare
     for j in range(len(data)):
         # data[j][num]=data[j][num].reshape(128,128)
-        axs[j].imshow(data[j][num].reshape(128,128).T, aspect='auto', interpolation='nearest',
+        axs[j].imshow(data[j][num].reshape(size,size).T, aspect='auto', interpolation='nearest',
             vmin=-2, vmax=2, cmap='gray',
             extent=(x.min(), x.max(), t.max(), t.min()))
     if save:
