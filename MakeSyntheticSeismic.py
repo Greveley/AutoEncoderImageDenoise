5b4f5b64055ff10ee4e8eea6356123ffa8546fec
greveley <66331857+Greveley@users.noreply.github.com>
2021-03-31 16:45:43 +0100

    editing synthetic data to be steeper and higher freq

--- MakeSyntheticSeismic.py
+++ MakeSyntheticSeismic.py
@@ -3,6 +3,84 @@ import matplotlib.pyplot as plt
 from pylops.utils.seismicevents import makeaxis, linear2d
 from pylops.utils.wavelets import ricker
 import random
+from skimage.util import random_noise
+
+def MakeSeismic_VN(samples,img_size=256,num_events=10):
+
+    """Simple generation of noisy synthetic linear seismic events. 
+        Input:
+        samples =  Number of samples in your dataset you want
+        
+        Output: 
+        clean_signal, noise, noisy_signal"""
+    random.seed(101)
+    # empty list to be filled with numpy arrays
+    clean_signal = []
+    noise = []
+    noisy_signal = []
+    # Parameters for the seismic canvas
+    par = {'ox':0, 'dx':12.5, 'nx':img_size, # offsets
+       'ot':0, 'dt':0.004, 'nt':img_size, # time
+       'f0': random.randint(5,50), 'nfmax': 50}
+       # initial tests, max freq was 30
+    # Make canvas
+    t, t2, x, y = makeaxis(par)
+    # Make wavelet
+    wav = ricker(np.arange(41) * par['dt'],f0=par['f0'])[0]
+    # Parameters for events
+    v = 1500
+    # orig amp range was 50
+    ang_range = 80
+    amp_range = 2
+    i = 0
+    amp_lim = 0.8
+    while i < samples: 
+        iEv = 0
+        t0 = []
+        theta = []
+        amp = []
+        while iEv <= num_events:
+            # Time of events
+            t0.append(random.uniform(t.min(),t.max())*0.7) 
+            # Angle of events
+            theta.append(random.uniform(-ang_range,ang_range))
+            # Amplitude of events
+            amp.append(random.uniform(-amp_range,amp_range))
+            # clipping events to be above -0.2 and 0.2 
+            if amp[iEv]<0:
+                amp[iEv] = np.min([-amp_lim,amp[iEv]])
+            else: 
+                amp[iEv] = np.max([amp_lim,amp[iEv]])
+            iEv+=1
+        
+        # Making events
+        mlin, mlinwav = linear2d(x, t, v, t0,theta, amp, wav)
+        s = mlinwav
+         # Creating and adding noise
+        ns1 = random_noise(s,'speckle',clip=False,var=random.uniform(0.2,2))
+        ns2 = random_noise(s,'gaussian',clip=False,var=random.uniform(0.05,0.5))
+        ns3 = random_noise(s,'s&p',clip=False,amount=random.uniform(0.05,0.2))
+
+        # Noise
+        n1 = ns1 - s
+        n2 = ns2 - s
+        n3 = ns3 - s
+
+        clean_signal.append(s)
+        clean_signal.append(s)
+        clean_signal.append(s)
+
+        noise.append(n1)
+        noise.append(n2)
+        noise.append(n3)
+
+        noisy_signal.append(ns1)
+        noisy_signal.append(ns2)
+        noisy_signal.append(ns3)
+
+        i +=1
+
+    return  np.array(clean_signal).reshape(samples*3,img_size,img_size,1),np.array(noise).reshape(samples*3,img_size,img_size,1),np.array(noisy_signal).reshape(samples*3,img_size,img_size,1)
 
 def MakeSeismic(samples,img_size=128,freq_low=5,freq_high=30,num_events=6):
 
