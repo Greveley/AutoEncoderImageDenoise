bc9caf209a2c71dcc15fc7c4dd41c01fc4ece5a1
greveley <66331857+Greveley@users.noreply.github.com>
2021-04-08 17:03:09 +0100

    Trying average pooling

--- ModelGenSeis.py
+++ ModelGenSeis.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,Conv2D,MaxPool2D,Dropout,UpSampling2D,Input,MaxPooling2D,Conv2DTranspose,concatenate,BatchNormalization,ReLU
+from tensorflow.keras.layers import Dense,Conv2D,MaxPool2D,AveragePooling2D,Dropout,UpSampling2D,Input,MaxPooling2D,Conv2DTranspose,concatenate,BatchNormalization,ReLU
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras import Model
 from tensorflow.keras.regularizers import l2
@@ -121,39 +121,6 @@ def autoencoder(type, start_filters=8, kernel=(3,3),input_size=(128,128,1)):
 
         model = Model(input_layer,output_layer)
 
-    if type=="Unet_v2":
-        input_shape = input_size
-        input_layer = Input(shape=(input_shape))
-        # # LHS of UNET
-        conv1 = Conv2D(start_filters * 1, kernel, activation="relu", padding="same")(input_layer)  
-        conv1 = Conv2D(start_filters * 1, kernel, activation="relu", padding="same")(conv1)
-        pool1 = MaxPooling2D((2, 2))(conv1)                                                         
-        pool1 = Dropout(0.25)(pool1)
-
-        conv2 = Conv2D(start_filters * 4, kernel, activation="relu", padding="same")(pool1)
-        conv2 = Conv2D(start_filters * 4, kernel, activation="relu", padding="same")(conv2)
-        pool2 = MaxPooling2D((2, 2))(conv2)                                                        
-        pool2 = Dropout(0.5)(pool2)
-
-        # # Middle
-        convm = Conv2D(start_filters * 16, kernel, activation="relu", padding="same")(pool2)
-        convm = Conv2D(start_filters * 16, kernel, activation="relu", padding="same")(convm)
-
-        deconv2 = Conv2DTranspose(start_filters * 4, kernel, strides=(2, 2), padding="same")(convm)
-        uconv2 = concatenate([deconv2, conv2])
-        uconv2 = Dropout(0.5)(uconv2)
-        uconv2 = Conv2D(start_filters * 4, kernel, activation="relu", padding="same")(uconv2)
-        uconv2 = Conv2D(start_filters * 4, kernel, activation="relu", padding="same")(uconv2)
-
-        deconv1 = Conv2DTranspose(start_filters * 1, kernel, strides=(2, 2), padding="same")(uconv2)
-        uconv1 = concatenate([deconv1, conv1])
-        uconv1 = Dropout(0.5)(uconv1)
-        uconv1 = Conv2D(start_filters * 1, kernel, activation="relu", padding="same")(uconv1)
-        uconv1 = Conv2D(start_filters * 1, kernel, activation="relu", padding="same")(uconv1)
-
-        output_layer = Conv2D(1, (1,1), padding="same", activation="linear")(uconv1)
-
-        model = Model(input_layer,output_layer)
 
     elif type == 'upsc_v2':
         model = Sequential()
@@ -183,10 +150,43 @@ def autoencoder(type, start_filters=8, kernel=(3,3),input_size=(128,128,1)):
         model.add(Conv2D(filters = start_filters*4, kernel_size = kernel,padding = 'Same', 
                         activation ='relu'))
         model.add(UpSampling2D((2,2)))
+        model.add(Conv2D(filters = start_filters*2, kernel_size = kernel,padding = 'Same', 
+                        activation ='relu'))
+        model.add(Conv2D(1,(3,3),activation='linear',padding='same'))
+
+    elif type == 'upsc_v2_avg':
+        model = Sequential()
+        model.add(Conv2D(filters = start_filters*2, kernel_size = kernel,padding = 'Same', 
+                        activation ='relu',input_shape=input_size))
+        model.add(AveragePooling2D(pool_size=(2,2),padding='same'))
+        # model.add(Dropout(0.2))
         model.add(Conv2D(filters = start_filters*4, kernel_size = kernel,padding = 'Same', 
+                        activation ='relu'))
+        model.add(AveragePooling2D(pool_size=(2,2),padding='same'))
+        # model.add(Dropout(0.2))
+
+        model.add(Conv2D(filters = start_filters*8, kernel_size = kernel,padding = 'Same', 
+                        activation ='relu'))
+        model.add(AveragePooling2D(pool_size=(2,2),padding='same'))
+        # model.add(Dropout(0.2))
+
+        model.add(Conv2D(filters = start_filters*16, kernel_size = kernel,padding = 'Same', 
+                        activation ='relu'))
+        model.add(Conv2D(filters = start_filters*16, kernel_size = kernel,padding = 'Same', 
+                        activation ='relu'))
+
+        model.add(UpSampling2D((2,2)))
+        model.add(Conv2D(filters = start_filters*8, kernel_size = kernel,padding = 'Same', 
+                        activation ='relu'))
+        model.add(UpSampling2D((2,2)))
+        model.add(Conv2D(filters = start_filters*4, kernel_size = kernel,padding = 'Same', 
+                        activation ='relu'))
+        model.add(UpSampling2D((2,2)))
+        model.add(Conv2D(filters = start_filters*2, kernel_size = kernel,padding = 'Same', 
                         activation ='relu'))
 
         model.add(Conv2D(1,(3,3),activation='linear',padding='same'))
+
 
     elif type == 'upsc_v3':
         model = Sequential()
